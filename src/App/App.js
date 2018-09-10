2d2de785e328f9e0817c1f9d2c765af8024e3cee
dinobaggio <dino.baggio007@gmail.com>
2018-09-10 15:22:45 +0700

    first todo commit

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todo from './component/Todo';
 
 class App extends Component {
   render() {
     return (
       <div>
-          <h1>Hallo World</h1>
+          <Todo />
       </div>
     );
   }
